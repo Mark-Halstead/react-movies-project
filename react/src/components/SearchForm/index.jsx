a3246c6db91bc3ac90b119f2b0300a28ded55ea8
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-02-27 11:03:20 +0000

    started working on form

--- react/src/components/SearchForm/index.jsx
+++ react/src/components/SearchForm/index.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const SearchForm = () => {
+
+    const [input, setInput] = useState("")
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
   return (
-    <div>SearchForm</div>
+    <form className='search-form' onSubmit={(e) => e.preventDefault()}>
+      <h2>Search Movies</h2>
+      <input
+        type='text '
+        className='form-input'
+        value=""
+        onChange={handleChange}
+      />
+      {error.show && <div className='error'>{error.msg}</div>}
+    </form>
   )
 }
 
