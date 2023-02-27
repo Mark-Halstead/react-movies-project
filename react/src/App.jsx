a45e50e16a29cd75035470e1c30179c098585dcd
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-02-27 10:10:44 +0000

    trying to get the routing working

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,22 @@
 import react from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts'
 import ProtectedRoute from './routes'
 import Home from './pages/Home'
 import Movies from './pages/Movies'
 import './App.css'
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/movies/:id" element={<Movies />}></Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<ProtectedRoute redirectTo="/" />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+        <Route path="/movies/:id" element={<Movies />}></Route>
+        <Route path="*" element={<h1>Not found</h1>}></Route>
+      </Routes>
+    </AuthProvider>
   )
 }
 
--- react/src/pages/Movies/index.jsx
+++ react/src/pages/Movies/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Movies = () => {
   return (
-    <div>
-        
-    </div>
+    <>
+
+    </>
   )
 }
 
