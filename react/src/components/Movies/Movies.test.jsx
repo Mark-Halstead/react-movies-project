c7ede11c3b54e18012f93566c1deb90f2dd02828
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-02-27 11:09:17 +0000

    added the bare bones of the test files

---
+++ react/src/components/Movies/Movies.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { screen, render, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+import { BrowserRouter } from 'react-router-dom'
+
+import matchers from '@testing-library/jest-dom/matchers';
+expect.extend(matchers);
+
+
+import Movies from './index';
