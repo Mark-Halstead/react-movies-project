8837f2fc437ef2cd99bb37079d31950a717491b7
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-02-27 09:50:29 +0000

    add some more code

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,14 @@
-import react, { useState } from 'react'
+import react from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/movies/:id" element={<Movie />}></Route>
+    </Routes>
   )
 }
 
